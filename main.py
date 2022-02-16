d72010f93217762445b74df8fcfb567d6ad9cbc6
vera <lerika250293@mail.ru>
2022-02-16 13:36:55 +0300

    all ready

--- main.py
+++ main.py
@@ -1,70 +1,59 @@
 import json
 from datetime import date
-with open("data.json","r" , encoding='utf-8') as f:
-        place = json.load(f)
+
+with open("data.json", "r", encoding='utf-8') as f:
+    place = json.load(f)
 dt_now = date.today()
-day = dt_now.weekday()+1
+day = dt_now.weekday() + 1
 week_day = {
-        1: "понедельник",
-        2: "вторник",
-        3: "среда",
-        4: "четверг",
-        5: "пятница",
-        6: "суббота",
-        7: "воскресенье",
+    1: "понедельник",
+    2: "вторник",
+    3: "среда",
+    4: "четверг",
+    5: "пятница",
+    6: "суббота",
+    7: "воскресенье",
 
 }
 for num, days in week_day.items():
     if num == day:
         cur_day = week_day[num]
 
+
 def example():
+    while True:
+        totals = []
+        choose_otdel = {}
+        for otdel in place:
+            print(f"{otdel['District']}")
+        choose = str(input("Выберите район:  "))
+        choose = choose.title()
+        choose_otdel["District"] = choose
+        for otdel in place:
+            for key, value in otdel.items():
+                if str(choose) in str(value):
+                    totals.append(otdel["District"])
+                    totals.append(otdel["ShortName"])
+                    totals.append(otdel["Address"])
+
+                    for spisok in otdel['PublicPhone']:
+                        for phone, numbers in spisok.items():
+                            totals.append(str(numbers))
 
 
-        while True:
-                choose_otdel = {}
-                phone_numbers = []
-                for otdel in place:
-                    print(f"{otdel['District']}")
-                choose = str(input("Выберите район:  "))
-                choose.split()
-                choose = choose.title()
-                if choose not in "Дмитровский":
-                        total = 'район' + ' ' + choose
-                else:
-                        total = choose + ' ' + 'район'
-                choose_otdel["District"]=total
-                try:
-                    for otdel in place:
-                            for k, v in choose_otdel.items():
-                                if otdel[k] == total:
-                                    print(otdel["ShortName"])
-                                    print(otdel["Address"])
-                                else:
-                                    break
 
-                            for k, v in choose_otdel.items():
+                    for spisok in otdel['WorkingHours']:
+                        for key, days in spisok.items():
+                            if spisok[key] == cur_day:
+                                totals.append(cur_day)
+                                totals.append(spisok["Hours"])
+                                totals.append(otdel["ClarificationOfWorkingHours"])
+                                break
 
-                                if otdel[k] == total:
-
-                                    for spisok in otdel['PublicPhone']:
-                                        for phone, numbers in spisok.items():
-                                            print(numbers)
-                                    for k, v in choose_otdel.items():
-                                        for spisok in otdel['WorkingHours']:
-                                            for key, days in spisok.items():
-                                                if spisok[key] == cur_day:
-                                                    for k, d in spisok.items():
-                                                        print(d)
-                                                    break
-                except EOFError:
-                    print('Выберите арйон из списка')
-                else:
-                    break
-
-
+        if len(totals) == 0:
+            print('Error! Выберите отдел из списка!')
+        else:
+            for i in totals:
+                print(i)
 
 example()
-
-
-
